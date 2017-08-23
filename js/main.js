036cc50c9a773342020587a71c0b76aaea253f70
Skylar Youmans <skyoumans@hotmail.com>
2017-08-23 14:21:13 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+if (cardsInPlay.length === 2) {
+	console.log("Two cards are in play");
+}
+if (cardsInPlay[0] === cardsInPlay[2]) 
+	alert("You found a match.");
+else
+	alert("Sorry, try again.");
